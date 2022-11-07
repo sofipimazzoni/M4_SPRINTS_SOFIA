63bae6e795f593a7f812e18319a88fd799bd9218
sofipimazzoni <83848719+sofipimazzoni@users.noreply.github.com>
2022-11-07 16:09:44 -0300

    exercicio final

--- SEMANA_03/EXERCICIOS/exercicio 2 - código circuito/exercicio2.ino
+++ SEMANA_03/EXERCICIOS/exercicio 2 - código circuito/exercicio2.ino
@@ -9,7 +9,7 @@ const int button1 = 13;
 const int button2 = 48;
 int storege_values[100] = {};
 int counter = 0;
-const int buzzer = 20;
+const int buzzer = 37;
 
 void setup() {
   // put your setup code here, to run once:
@@ -21,7 +21,9 @@ void setup() {
   pinMode(LED4, OUTPUT);
   pinMode(button1, INPUT_PULLUP);
   pinMode(button2, INPUT_PULLUP);
+  pinMode(buzzer, OUTPUT);
 }
+
 void loop() {
   // put your main code here, to run repeatedly:
   delay(10); // this speeds up the simulation
@@ -31,8 +33,10 @@ void loop() {
   int LDR_second_value = map(LDR_first_value, inicial_value, final_value, 0, 15);
 
   Serial.println(LDR_second_value);
+  int button1_value = digitalRead(button1);
+  Serial.printf("leitura button1 = %i\n",button1_value);
 
-  if(analogRead(button1) == LOW){
+  if(button1_value == 0){
     storege_values[counter] = LDR_second_value;
     counter += 1;
 
@@ -43,17 +47,24 @@ void loop() {
 
     tone(buzzer, 300 + (100 * LDR_second_value), 1000);
     delay(1000);
+
+    button1_value = 1;
   }
 
   int button2_value = digitalRead(button2);
-  if(analogRead(button2) == LOW){
+  Serial.printf("leitura button2 = %i\n",button2_value);
+
+  if(button2_value == 0){
     for(int i = 0; i < counter; i++){
       digitalWrite(LED1, 1 & storege_values[i] >> 0);
       digitalWrite(LED2, 1 & storege_values[i] >> 1);
       digitalWrite(LED3, 1 & storege_values[i] >> 2);
       digitalWrite(LED4, 1 & storege_values[i] >> 3);
+
       tone(buzzer, 300 + (100 * storege_values[i]), 1000);
       delay(1100);
+
+      button2_value = 1;
     }
   }
   digitalWrite(LED1, LOW);
